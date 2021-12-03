7058a71f4359a3efa2b4f4f72936af3b5b13cb03
Andres Talavera <andres.talavera@live.fr>
2021-12-03 17:35:03 +0100

    Update Carousel model

--- GalerieRenovation.Models/Models.cs
+++ GalerieRenovation.Models/Models.cs
@@ -6,6 +6,6 @@ public record Service(string Seo,
     string Description2,
     IEnumerable<string> Descriptions,
     IEnumerable<string> ShortDescriptions);
-public record Carousel(IEnumerable<CarouselItem> Items);
-public record CarouselItem(string UniqueId, string Description, string ImageUrl);
+public record Carousel(string UniqueId, IEnumerable<CarouselItem> Items);
+public record CarouselItem(string Description, string ImageUrl);
 public record Informations(string Url);
