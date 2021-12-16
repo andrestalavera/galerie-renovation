d635310ca3cf6b0c6656397ca8b3b72e8e3539ea
Andres Talavera <andres.talavera@live.fr>
2021-12-16 14:01:57 +0100

    Navbar improvments

--- GalerieRenovation.Web/gruntfile.js
+++ GalerieRenovation.Web/gruntfile.js
@@ -1,11 +1,11 @@
-/// <binding BeforeBuild='clean, sass, cssmin' Clean='clean' />
+/// <binding BeforeBuild='clean, sass, cssmin, uglify' Clean='clean' />
 module.exports = function (grunt) {
     grunt.initConfig({
         // a task to know which files are edited 
         watch: ["wwwroot/scss/*", "wwwroot/ts/*"],
 
         // delete generated files
-        clean: ["wwwroot/css/*", "wwwroot/js/*"],
+        clean: ["wwwroot/css/*", "pages/*.css", "shared/*.css", "wwwroot/js/*.js", "!wwwroot/js/*.min.js"],
 
         // compile sass to css with map
         sass: {
@@ -24,7 +24,7 @@ module.exports = function (grunt) {
             },
             gr: {
                 files: {
-                    //'wwwroot/js/hello.min.js': ['wwwroot/js/hello.js']
+                    'wwwroot/js/header-scroll.min.js': ['wwwroot/js/header-scroll.js']
                 }
             }
         },
