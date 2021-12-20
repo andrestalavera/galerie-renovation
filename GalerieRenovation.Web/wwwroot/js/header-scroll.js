f26f8fec86b90cfc53790121a1c8143c08a7844e
Andres Talavera <andres.talavera@live.fr>
2021-12-20 13:22:07 +0100

    remove logs

--- GalerieRenovation.Web/wwwroot/js/header-scroll.js
+++ GalerieRenovation.Web/wwwroot/js/header-scroll.js
@@ -3,13 +3,10 @@ function scrollFunction() {
     let imageBrand = document.getElementById("image-brand");
     let main = document.getElementById("main");
     let expandedClassName = "expanded";
-    console.log(imageBrand, main, document.documentElement.scrollTop);
     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-        console.log("> 50");
         imageBrand.classList.remove(expandedClassName);
         main.classList.remove(expandedClassName);
     } else {
-        console.log("< 50");
         imageBrand.classList.add(expandedClassName);
         main.classList.add(expandedClassName);
     }
