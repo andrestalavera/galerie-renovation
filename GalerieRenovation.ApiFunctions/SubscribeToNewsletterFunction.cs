8489eee3a6e0afd0b2c4b10714aa8f6a0896ce8a
Andres Talavera <andres.talavera@live.fr>
2022-05-05 18:19:46 +0200

    add a function to subscribe to the newsletter

---
+++ GalerieRenovation.ApiFunctions/SubscribeToNewsletterFunction.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using System;
+using System.Threading.Tasks;
+
+namespace GalerieRenovation.ApiFunctions;
+
+public static class SubscribeToNewsletterFunction
+{
+    [FunctionName(nameof(SubscribeToNewsletterFunction))]
+    public static async Task<IActionResult> Run(
+        [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest request)
+    {
+        throw new NotImplementedException();
+    }
+}
