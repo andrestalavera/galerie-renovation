5f94ada19a788cdfabd396ff7201e43e11896c68
Andres Talavera <andres.talavera@live.fr>
2022-05-06 09:27:32 +0200

    update contact form ui with BlazorBootstrap

--- GalerieRenovation.Web/Program.cs
+++ GalerieRenovation.Web/Program.cs
@@ -1,3 +1,4 @@
+using BlazorBootstrap;
 using GalerieRenovation.Web;
 using GalerieRenovation.Web.Services;
 using Microsoft.AspNetCore.Components.Web;
@@ -8,16 +9,13 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddHttpClient("data", hc => hc.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
-string? contactFunctionUrl = builder.Configuration["CONTACTFORM_FUNCTION"];
-if (string.IsNullOrEmpty(contactFunctionUrl))
-{
-    ArgumentNullException.ThrowIfNull(nameof(contactFunctionUrl));
-}
-else
-{
-    builder.Services.AddHttpClient("contact", hc => hc.BaseAddress = new Uri(contactFunctionUrl));
-}
+string? contactFunctionUrl = builder.Configuration["FUNCTION_API_BASEURL"];
+ArgumentNullException.ThrowIfNull(contactFunctionUrl);
+builder.Services.AddHttpClient("contact", hc => hc.BaseAddress = new Uri(contactFunctionUrl));
 builder.Services.AddScoped<IContactService, ContactService>();
-
+builder.Services.AddBlazorBootstrap();
+string? emailFunctionUrl = builder.Configuration["FUNCTION_API_BASEURL"];
+ArgumentNullException.ThrowIfNull(emailFunctionUrl);
+builder.Services.AddHttpClient("api", hc => hc.BaseAddress = new(emailFunctionUrl));
 
 await builder.Build().RunAsync();
